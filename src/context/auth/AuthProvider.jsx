b11e4d4f22583862ff0bc0152e1cb98a99f9128b
stefanRachev <starfystef@gmail.com>
2025-05-22 13:57:17 +0300

    feat: add auth context

---
+++ src/context/auth/AuthProvider.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged ,updateProfile } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+import { auth } from "../firebase/firebase"
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false); 
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const register = async (email, password,username) => {
+    try {
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+
+      await updateProfile(result.user, {
+        displayName: username,
+      });
+      return result;
+    } catch (error) {
+      console.error("Registration error:", error);
+      throw error; 
+    }
+  };
+  
+  const login = async (email, password) => {
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      return result;
+    } catch (error) {
+      console.error("Login error:", error);
+      throw error;
+    }
+  };
+  
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error("Logout error:", error);
+      throw error;
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, register,loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
